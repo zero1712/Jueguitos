08a6a7f5ae85430ee0e37187241f8d00f93f9a30
jonas <jonas@jonas-MacBookPro>
2016-12-05 22:40:50 -0600

    jueguitos package

---
+++ src/java/jueguitos/Fizz.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jueguitos;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Programa4
+ */
+@WebServlet(description = "Fizz Bazz Buzz", urlPatterns = { "/Fizz" })
+public class Fizz extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Fizz() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		int i=1;
+		PrintWriter out=response.getWriter();
+		out.println("Fizz Bazz Buzz");
+		out.println();
+		out.println(i);
+		++i;
+		while (i<=50){			
+			if (i%3==0 && i%5==0){
+				out.println(i+" Buzz");
+			}else if(i%3==0){
+				out.println(i+" Fizz");
+			}else if(i%5==0){
+				out.println(i+" Bazz");
+			}else{
+				out.println(i);
+			}
+			i++;
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
