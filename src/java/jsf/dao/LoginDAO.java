84b935907ecd538b6dcee1163d4e1420d1d0604d
Zombietihu <tonatihu.fi@gmail.com>
2016-12-06 17:41:55 -0600

    hol

--- src/java/jsf/dao/LoginDAO.java
+++ src/java/jsf/dao/LoginDAO.java
@@ -55,4 +55,90 @@ public static int validate(String user, String password) {
 		}
 		return 100;
 	}
+public static int claveUsuario(String user){
+    Connection con = null;
+		PreparedStatement ps = null;
+                Statement stm;
+                ResultSet rs;
+                String sql = "select clave from usuarios where login ='"+user+"'";
+                System.out.println("Usuario: "+user);
+		try {
+			/**con = DataConnect.getConnection();
+			ps = con.prepareStatement("select login, password from usuarios where login ="+user+" and password ="+password);
+			//ps.setString(1, user);
+			//ps.setString(2, password);
+
+			ResultSet rs = ps.executeQuery();
+
+			if (rs.next()) {
+				//result found, means valid inputs
+				return true;
+                                * 
+			}**/
+                        
+                        stm= DataConnect.getConnection().createStatement();
+                        rs=stm.executeQuery(sql);
+                        if (rs.next()) {
+				//result found, means valid inputs
+                                int tipo= rs.getInt("clave");
+				return tipo;
+                                
+			}
+		} catch (SQLException ex) {
+			System.out.println("Login error -->" + ex.getMessage());
+			return 100;
+		} finally {
+			DataConnect.close(con);
+		}
+		return 100;
+    
+}
+public static boolean actualizaRecord(int j, int usuario){
+                Connection con = null;
+		PreparedStatement ps = null;
+                PreparedStatement ps2 = null;
+                Statement stm;
+                ResultSet rs;
+                String sql = "update records set record=? where cve_juego =? and cve_usuario =?";
+                String sql2 = "select record from records where cve_usuario="+usuario;
+                String sql3 = "insert into records(cve_juego,cve_usuario,record) values(?,?,?)";
+                
+                System.out.println("cve_Juego: "+j+" cve_usuario: "+usuario);
+                try{
+                    con = DataConnect.getConnection();
+                    ps = con.prepareStatement(sql2);
+                    
+                    stm= DataConnect.getConnection().createStatement();
+                        rs=stm.executeQuery(sql2);
+                        if (rs.next()) {
+				//result found, means valid inputs
+                                int tipo= rs.getInt("record");
+                                
+				ps = con.prepareStatement(sql);
+                                ps.setInt(1,tipo+1);
+                                ps.setInt(2, j);
+                                ps.setInt(3, usuario);
+                    
+                                int n = ps.executeUpdate();
+                                if(n>0)
+                                    return true;    
+                                
+                        }
+                        ps2 = con.prepareStatement(sql3);
+                        ps2.setInt(1, j);
+                        ps2.setInt(2,usuario);
+                        ps2.setInt(3, 1);
+                        boolean n = ps2.execute();
+                                if(n)
+                                    return true; 
+                    
+                    
+                }catch (SQLException ex) {
+			System.out.println("Update error -->" + ex.getMessage());
+			return false;
+		} finally {
+			DataConnect.close(con);
+		}
+                return false;
+}
 }
